002fa9b4da412d60ece1b46f984c9e0347199aab
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-08-20 20:51:49 +0200

    Controller and routes created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+const adminRoute = require('./routes/admin')
 require('dotenv').config();
 
 const app = express();
@@ -15,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/static', express.static(path.join(`${__dirname}/public`)));
 
-app.get('/', (req, res) => res.send('Home Route'));
+app.use('/', adminRoute);
 
 const port = process.env.PORT || 8080;
 
