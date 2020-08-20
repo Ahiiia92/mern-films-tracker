1778d50d51c6b7aa0a2c4ae5c1c7778133c867c1
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-08-20 21:59:46 +0200

    CRU done

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,38 @@
-exports.getIndex = (req, res) => {
-  res.status(200).render('index');
+const Film = require('../models/Film');
+
+exports.getIndex = async (req, res) => {
+  const film = await Film.find((data) => data);
+
+  try {
+    console.log(film);
+    res.status(200).render('index', { film: film });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getFilm = async (req, res) => {
+  const filmId = req.params.filmId;
+
+  const film = await Film.findById(filmId, (film) => film);
+
+  try {
+    console.log(film);
+    res.status(200).render('film', { film: film });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getAddFilm = (req, res) => {
+  res.status(200).render('edit-film');
+};
+
+exports.postFilm = (req, res) => {
+  const { name, image, description } = req.body;
+
+  const film = new Film({ name: name, image: image, description: description });
+  film.save();
+  console.log('Film Added to the database');
+  res.status(201).redirect('/');
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,10 @@ const router = express.Router();
 
 router.get('/', adminController.getIndex);
 
+router.get('/add-film', adminController.getAddFilm);
+router.post('/add-film', adminController.postFilm);
+
+router.get('/:filmId', adminController.getFilm);
+
 module.exports = router;
 
