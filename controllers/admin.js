4cefbfb5b92711fc1450eded492d41c599be759d
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-08-20 22:48:40 +0200

    Update functions added and associated views

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,7 +25,29 @@ exports.getFilm = async (req, res) => {
 };
 
 exports.getAddFilm = (req, res) => {
-  res.status(200).render('edit-film');
+  res.status(200).render('edit-film', { editing: false });
+};
+
+exports.getEditFilm = async (req, res) => {
+  const filmId = req.params.filmId;
+
+  const editMode = req.query.edit;
+
+  if (!editMode) {
+    return res.redirect('/');
+  }
+
+  const film = await Film.findById(filmId);
+
+  try {
+    if (!filmId) {
+      return res.redirect('/');
+    }
+    console.log(film);
+    res.status(200).render('edit-film', { film: film, editing: editMode });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.postFilm = (req, res) => {
@@ -36,3 +58,39 @@ exports.postFilm = (req, res) => {
   console.log('Film Added to the database');
   res.status(201).redirect('/');
 };
+
+exports.postEditFilm = (req, res) => {
+  const filmId = req.body.filmId;
+  const { name, image, description } = req.body;
+
+  Film.findById(filmId)
+    .then((film) => {
+      film.name = name;
+      film.image = image;
+      film.description = description;
+
+      return film.save();
+    })
+    .then(() => {
+      console.log('Item Updated');
+      res.status(201).redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postDelete = async (req, res) => {
+  const filmId = req.body.filmId;
+
+  const film = await Film.findByIdAndDelete(filmId, (data) => data);
+
+  try {
+    console.log(film);
+    console.log('Film deleted');
+    res.redirect('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,16 @@ const router = express.Router();
 router.get('/', adminController.getIndex);
 
 router.get('/add-film', adminController.getAddFilm);
+
+router.get('/edit-film/:filmId', adminController.getEditFilm);
+
 router.post('/add-film', adminController.postFilm);
 
+router.post('/edit-film', adminController.postEditFilm);
+
 router.get('/:filmId', adminController.getFilm);
+
+router.post('/delete', adminController.postDelete);
 
 module.exports = router;
 
